188424562b2eda675c8b638baa261747db95afd3
devdel2 <ddeleon2@uccs.edu>
2024-03-31 14:40:19 -0600

    started to add functionality to discord-test branch, need to figure out module.exports

--- app.js
+++ app.js
@@ -10,11 +10,36 @@ const compiledFunc = pug.compileFile('./views/layout.pug');
 // Path support
 const path = require('path');
 
+// JSON Support
+app.use(express.json());
+
+// Discord.js Setup
+const { Client, Intents } = require('discord.js');
+const  discordb  = require('./public/js/discord-bot');
+
+console.log(discordb.SayHello)
+
 // Default test get route
 app.get('/', (req, res) => {
     res.send(compiledFunc({
         path: 'Devin'
     }));
+});
+
+// Get method to get the target user's curernt status
+app.get('/user-status/:userID', async (req, res) => {
+    const { userID } = req.params;
+    try{
+        const user = await client.users.fetch(userID);
+        const status = user.presense.status;
+        res.status(200).json({ status });
+    }
+    catch (err) {
+        //internal err message
+        console.error(`Error fetching user status: ${err}`);
+        //rendered err message
+        res.status(500).send(`Error fetching user status.`);
+    }
 });
 
 // broadcast message on router
