cb6f9dc7cbe14acafef0eb3ab2db8f9a9ee2ccb9
devdel2 <ddeleon2@uccs.edu>
2024-03-31 19:43:40 -0600

    getting closer with discord setup, have access code, can't exchange it for some reason

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
+
+const env = require('dotenv').config({ path:"./.ENV" });
+const axios = require('axios');
+const session = require('express-session');
+const fs = require('fs');
+const https = require('https');
+
 // EXPRESS SETUP
 const express = require('express');
 const app = express();
+const options = {
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem'),
+    passphrase: 'x1t6w3f8u!!!'
+};
+const server = https.createServer(options, app);
 const port = 3000;
+
+server.listen(port, () => {
+    console.log(`Server running on https://localhost:${port}`);
+})
 
 // view engine support - NEED TO MOVE OUT OF THIS JS
 const pug = require('pug');
@@ -13,11 +30,75 @@ const path = require('path');
 // JSON Support
 app.use(express.json());
 
-// Discord.js Setup
-const { Client, Intents } = require('discord.js');
-const  discordb  = require('./public/js/discord-bot');
+//session support
+app.use(session({
+    secret: 'your_session_secret',
+    resave: false,
+    saveUninitialized: true
+}))
 
-console.log(discordb.SayHello)
+// DISCORD SETUP
+
+//client secret and ID
+const clientSecret = env.parsed.CLIENT_SECRET;
+const clientID = env.parsed.CLIENT_ID;
+const redirectURI = env.parsed.REDIRECT_URI;
+
+console.log(redirectURI)
+
+app.get('/DiscordAuth', (req, res) => {
+    const scopes = 'identify';
+    const discordOAuthUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientID}&redirect_uri=${encodeURIComponent(redirectURI)}&response_type=code&scope=${encodeURIComponent(scopes)}`;
+    res.redirect(discordOAuthUrl);
+});
+
+app.get('/ChangeDiscordStatus', async (req, res) => {
+    console.log(req.query);
+    const { code } = req.query;
+    if(!code) {
+        return res.status(400).send('Authorization code not provided');
+    }
+
+    try {
+        const response = await axios.post('https://discord.com/api/oauth2/token', {
+            client_id: clientID,
+            client_secret: clientSecret,
+            grant_type: 'authorization_code',
+            code,
+            redirect_uri: redirectURI,
+            scope: 'identify'
+        });
+        const { access_token } = response.data;
+
+        req.session.access_token = access_token;
+
+        // USE ACCESS TOKEN HERE TO CHANGE STATUS
+        //CHANGE STATUS TO ONLINE
+        // await axios.patch('https://discord.com/api/v9/users/@me/settings', {
+        //     status: 'online'
+        // }, {
+        //     headers: {
+        //         Authorization: `Bearer ${access_token}`
+        //     }
+        // });
+
+        res.redirect('/success');
+    }
+    catch (err) {
+        console.error('Error exchanging authorization code for access token: ', err);
+        res.status(500).send('Error exchanging authorization code for access token');
+    }
+});
+
+app.get('/success', (req,res) => {
+    const { access_token } = req.session;
+    if(!access_token){
+        return res.status(400).send('Access toekn not found in session');
+    }
+
+    res.send('Authorization successful! You can close this window.');
+})
+
 
 // Default test get route
 app.get('/', (req, res) => {
@@ -26,23 +107,7 @@ app.get('/', (req, res) => {
     }));
 });
 
-// Get method to get the target user's curernt status
-app.get('/user-status/:userID', async (req, res) => {
-    const { userID } = req.params;
-    try{
-        const user = await client.users.fetch(userID);
-        const status = user.presense.status;
-        res.status(200).json({ status });
-    }
-    catch (err) {
-        //internal err message
-        console.error(`Error fetching user status: ${err}`);
-        //rendered err message
-        res.status(500).send(`Error fetching user status.`);
-    }
-});
-
 // broadcast message on router
-app.listen(port, () => {
-    console.log(`example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`example app listening on port ${port}`);
+// });
--- public/js/discord-bot.js
+++ public/js/discord-bot.js
@@ -1,29 +1,2 @@
-require('dotenv').config();
 
-//bot token and channel ID
-const BOT_TOKEN = process.env.BOT_TOKEN;
 
-//data objects for Client and Intents from discord.js
-const { Client, Intents } = require('discord.js');
-
-const DiscordObject = [
-//client setup
-    function CreateClient(){
-        const client = new Client({
-            intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSSAGES],
-        })
-        //turning 'on' client, prints current user
-        client.on('ready', () => {
-            return console.log(`Logged in as ${client.user.tag}`);
-        });
-    },
-    function SayHello() {
-        return console.log("Hello");
-    }
-];
-
-console.log(DiscordObject);
-
-// client.login(BOT_TOKEN);
-
-module.exports = {DiscordObject};
